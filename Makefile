4e4006c21b7459e2b614713fdd08191427fa67f1
Demur Rumed <junkmute@hotmail.com>
2010-09-23 15:51:19 +0000

    Turns out gmp doesn't allow for mpn shifting more than a limb. Zooming works, as does auto precision. Odd limb boundaries solved by auto precision

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: pitman gabelsberger ogabelsberger zgabelsberger
+all: pitman gabelsberger
 pitman: pitman.c
 	gcc -std=gnu99 -fwhole-program -O3 -ffast-math -lGL -lpthread -march=native pitman.c -o pitman
 gabelsberger: gabelsberger.c
--- gabelsberger.c
+++ gabelsberger.c
@@ -12,30 +12,29 @@
 mp_limb_t*xx,*yy,*wh,*z4;
 _Bool xs=1,ys=1;
 unsigned char manor[512][512];
-unsigned mxi=300,q=59;
-mp_size_t pr;
-mp_limb_t mymp_lsh(mp_limb_t*r,mp_limb_t*s,mp_size_t p,unsigned n){
+unsigned mxi=300,q;
+mp_size_t pr=1;
+mp_limb_t mymp_lsh(mp_limb_t*r,mp_size_t p,unsigned n){
 	while(n>=mp_bits_per_limb){
-		mpn_lshift(r,s,p,mp_bits_per_limb-1);
+		mpn_lshift(r,r,p,mp_bits_per_limb-1);
 		n-=mp_bits_per_limb-1;
 	}
-	return mpn_lshift(r,s,p,n);
+	return mpn_lshift(r,r,p,n);
 }
-mp_limb_t mymp_rsh(mp_limb_t*r,mp_limb_t*s,mp_size_t p,unsigned n){
+mp_limb_t mymp_rsh(mp_limb_t*r,mp_size_t p,unsigned n){
 	while(n>=mp_bits_per_limb){
-		mpn_rshift(r,s,p,mp_bits_per_limb-1);
+		mpn_rshift(r,r,p,mp_bits_per_limb-1);
 		n-=mp_bits_per_limb-1;
 	}
-	return mpn_rshift(r,s,p,n);
+	return mpn_rshift(r,r,p,n);
 }
 void mymp_add(mp_limb_t*r,const mp_limb_t*a,const mp_limb_t*b,mp_size_t n,_Bool as,_Bool bs,_Bool*rs){
 	*rs=as;
 	if(as==bs) mpn_add_n(r,a,b,n);
-	else if(mpn_cmp(a,b,n)>=0) mpn_sub_n(r,a,b,n);
-	else{
+	else if(mpn_cmp(a,b,n)<0){
 		*rs=!as;
 		mpn_sub_n(r,b,a,n);
-	}
+	}else mpn_sub_n(r,a,b,n);
 }
 void*drawman(void*xv){
 	mp_limb_t zr[pr*2],zi[pr*2],ii[pr*2],i2[pr*2],cr[pr*2],ci[pr*2];
@@ -44,6 +43,7 @@ void*drawman(void*xv){
 	mymp_add(cr,cr,xx,pr,0,xs,&crs);
 	mpn_copyi(ci,yy,pr);
 	cis=ys;
+	unsigned q=pr*mp_bits_per_limb-5;
 	for(mp_limb_t j=0;j<512;j++){
 		zrs=crs;
 		mpn_copyi(zr,cr,pr);
@@ -54,18 +54,18 @@ void*drawman(void*xv){
 		do{
 			iis=zrs^zis;
 			mpn_mul_n(ii,zr,zi,pr);
-			mymp_rsh(ii,ii,pr*2,q-1);
+			mymp_rsh(ii,pr*2,q-1);
 			mpn_sqr(zr,zr,pr);
-			mymp_rsh(zr,zr,pr*2,q);
+			mymp_rsh(zr,pr*2,q);
 			mpn_sqr(zi,zi,pr);
-			mymp_rsh(zi,zi,pr*2,q);
+			mymp_rsh(zi,pr*2,q);
 			mymp_add(zr,zr,zi,pr,0,1,&zrs);
 			mymp_add(zr,zr,cr,pr,zrs,crs,&zrs);
 			mymp_add(zi,ii,ci,pr,iis,cis,&zis);
 			mpn_sqr(ii,zr,pr);
-			mymp_rsh(ii,ii,pr*2,q);
+			mymp_rsh(ii,pr*2,q);
 			mpn_sqr(i2,zi,pr);
-			mymp_rsh(i2,i2,pr*2,q);
+			mymp_rsh(i2,pr*2,q);
 			mpn_add_n(ii,ii,i2,pr);
 		}while(--k&&mpn_cmp(ii,z4,pr)<0);
 		((unsigned char*)xv)[j]=(k<<8)/mxi;
@@ -74,23 +74,21 @@ void*drawman(void*xv){
 int main(int argc,char**argv){
 	int nx,ny;
 	pthread_t a[THREADS];
-	pr=1+(q+4)/mp_bits_per_limb;
 	xx=calloc(pr*4,sizeof(mp_limb_t));
 	yy=xx+pr;
 	wh=yy+pr;
 	z4=wh+pr;
 	yy[0]=xx[0]=2;
 	wh[0]=z4[0]=4;
-	mymp_lsh(xx,xx,pr,q);
-	mymp_lsh(yy,yy,pr,q);
-	mymp_lsh(z4,z4,pr,q);
-	mymp_lsh(wh,wh,pr,q-9);
+	mymp_lsh(xx,pr,pr*mp_bits_per_limb-5);
+	mymp_lsh(yy,pr,pr*mp_bits_per_limb-5);
+	mymp_lsh(z4,pr,pr*mp_bits_per_limb-5);
+	mymp_lsh(wh,pr,pr*mp_bits_per_limb-14);
 	Display*dpy=XOpenDisplay(0);
 	XVisualInfo*vi=glXChooseVisual(dpy,DefaultScreen(dpy),(int[]){GLX_RGBA,None});
 	Window win=XCreateWindow(dpy,RootWindow(dpy,vi->screen),0,0,511,511,0,vi->depth,InputOutput,vi->visual,CWBorderPixel|CWColormap|CWEventMask,(XSetWindowAttributes[]){{.colormap=XCreateColormap(dpy,RootWindow(dpy,vi->screen),vi->visual,AllocNone),.border_pixel=0,.event_mask=ExposureMask|KeyPressMask|ButtonPressMask|ButtonReleaseMask}});
 	XMapWindow(dpy,win);
 	GLXContext ctx=glXCreateContext(dpy,vi,0,GL_TRUE);
-	XEvent event;
 	glXMakeCurrent(dpy,win,ctx);
 	glOrtho(0,511,511,0,1,-1);
 	pthread_attr_t pat;
@@ -103,11 +101,12 @@ int main(int argc,char**argv){
     pthread_setschedparam(pthread_self(),SCHED_RR,(struct sched_param[]){{.sched_priority=sched_get_priority_min(SCHED_RR)}});
 	goto rend;
 	for(;;){
-		XNextEvent(dpy,&event);
-		switch(event.type){
+		XEvent ev;
+		XNextEvent(dpy,&ev);
+		switch(ev.type){
 		case KeyPress:{
 			KeySym keysym;
-			if(XLookupString((XKeyEvent*)&event,(char[]){0},1,&keysym,NULL)==1&&keysym==XK_Escape){
+			if(XLookupString((XKeyEvent*)&ev,(char[]){0},1,&keysym,NULL)==1&&keysym==XK_Escape){
 				glXDestroyContext(dpy,glXGetCurrentContext());
 				return 0;
 			}
@@ -122,40 +121,89 @@ int main(int argc,char**argv){
 			glEnd();
 			glFlush();
 		break;case ButtonPress:
-			if(event.xbutton.button==Button1){
-				if((unsigned)event.xbutton.x>512||(unsigned)event.xbutton.y>512) break;
-				nx=event.xbutton.x;
-				ny=event.xbutton.y;
-			}else if(event.xbutton.button==Button4) mxi+=25;
-			else if(event.xbutton.button==Button5&&mxi>100) mxi-=25;
+			if(ev.xbutton.button==Button1){
+				if((unsigned)ev.xbutton.x>512||(unsigned)ev.xbutton.y>512) break;
+				nx=ev.xbutton.x;
+				ny=ev.xbutton.y;
+			}else if(ev.xbutton.button==Button4) mxi+=25;
+			else if(ev.xbutton.button==Button5&&mxi>100) mxi-=25;
 			else goto rend;
 		break;case ButtonRelease:
-			if(event.xbutton.button==Button1){
-				if((unsigned)event.xbutton.x>512||(unsigned)event.xbutton.y>512) break;
-				if(event.xbutton.x==nx&&event.xbutton.y==ny){
-					mpn_submul_1(xx,wh,pr,512-event.xbutton.x);
-					mpn_submul_1(yy,wh,pr,512-event.xbutton.y);
+			if(ev.xbutton.button==Button1){
+				if((unsigned)ev.xbutton.x>512||(unsigned)ev.xbutton.y>512) break;
+				if(ev.xbutton.x==nx&&ev.xbutton.y==ny){
+					if(xs){
+						mpn_addmul_1(xx,wh,pr,512-ev.xbutton.x);
+					}else{
+						mp_limb_t t[pr*2];
+						mpn_mul_1(t,wh,pr,512-ev.xbutton.x);
+						if(mpn_cmp(xx,t,pr)<0){
+							xs=!xs;
+							mpn_sub_n(xx,t,xx,pr);
+						}else mpn_sub_n(xx,xx,t,pr);
+					}
+					if(ys){
+						mpn_addmul_1(yy,wh,pr,512-ev.xbutton.y);
+					}else{
+						mp_limb_t t[pr*2];
+						mpn_mul_1(t,wh,pr,512-ev.xbutton.y);
+						if(mpn_cmp(yy,t,pr)<0){
+							ys=!ys;
+							mpn_sub_n(yy,t,yy,pr);
+						}else mpn_sub_n(yy,yy,t,pr);
+					}
 					mpn_lshift(wh,wh,pr,1);
 				}else{
-					if(event.xbutton.x<nx){
-						int t=event.xbutton.x;
-						event.xbutton.x=nx;
+					if(ev.xbutton.x<nx){
+						int t=ev.xbutton.x;
+						ev.xbutton.x=nx;
 						nx=t;
 					}
-					if(event.xbutton.y<ny){
-						int t=event.xbutton.y;
-						event.xbutton.y=ny;
+					if(ev.xbutton.y<ny){
+						int t=ev.xbutton.y;
+						ev.xbutton.y=ny;
 						ny=t;
 					}
-					mpn_addmul_1(xx,wh,pr,nx);
-					mpn_addmul_1(yy,wh,pr,ny);
-					nx=event.xbutton.x-nx;
-					ny=event.xbutton.y-ny;
+					if(!xs){
+						mpn_addmul_1(xx,wh,pr,nx);
+					}else{
+						mp_limb_t t[pr*2];
+						mpn_mul_1(t,wh,pr,nx);
+						if(mpn_cmp(xx,t,pr)<0){
+							xs=!xs;
+							mpn_sub_n(xx,t,xx,pr);
+						}else mpn_sub_n(xx,xx,t,pr);
+					}
+					if(!ys){
+						mpn_addmul_1(yy,wh,pr,ny);
+					}else{
+						mp_limb_t t[pr*2];
+						mpn_mul_1(t,wh,pr,ny);
+						if(mpn_cmp(yy,t,pr)<0){
+							ys=!ys;
+							mpn_sub_n(yy,t,yy,pr);
+						}else mpn_sub_n(yy,yy,t,pr);
+					}
+					nx=ev.xbutton.x-nx;
+					ny=ev.xbutton.y-ny;
 					mpn_mul_1(wh,wh,pr,nx-(nx-ny&nx-ny>>sizeof(int)*8-1));
 					mpn_rshift(wh,wh,pr,9);
+					if(xx[0]||yy[0]){
+						pr++;
+						xx=realloc(xx,pr*4*sizeof(mp_limb_t));
+						yy=xx+pr;
+						wh=yy+pr;
+						z4=wh+pr;
+						for(int i=pr-1;i>-1;i--){
+							z4[i]=z4[i-4];
+							wh[i]=wh[i-3];
+							yy[i]=yy[i-2];
+							xx[i]=xx[i-1];
+						}
+						xx[0]=yy[0]=wh[0]=z4[0]=0;
+					}
 				}
-				rend:;
-				gmp_printf("%u\n%Nx\n%Nx\n%Nx\n",q,xx,pr,yy,pr,wh,pr);
+				rend:gmp_printf("%u\n%Nx\n%Nx\n%Nx\n",pr,xx,pr,yy,pr,wh,pr);
 				uint32_t n[THREADS],mans=THREADS-1;
 				for(int i=0;i<THREADS;i++){
 					n[i]=i;
