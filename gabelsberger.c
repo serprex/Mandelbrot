64b7404190bcadfdaf71f1fb604bb960122d6285
Demur Rumed <junkmute@hotmail.com>
2010-09-24 21:00:05 +0000

    Asynchronous control flow. Only locks up now because of max thread priority

--- gabelsberger.c
+++ gabelsberger.c
@@ -57,11 +57,9 @@ void*drawman(void*xv){
 	}
 }
 int main(int argc,char**argv){
-	int nx,ny;
+	int nx,ny,n[THREADS],mans=THREADS-1;
 	pthread_t a[THREADS];
-	xx=calloc(3,sizeof(mp_limb_t));
-	yy=xx+1;
-	wh=yy+1;
+	wh=(yy=(xx=malloc(3*sizeof(mp_limb_t)))+1)+1;
 	wh[0]=(yy[0]=xx[0]=2ULL<<mp_bits_per_limb-5)>>8;
 	Display*dpy=XOpenDisplay(0);
 	XVisualInfo*vi=glXChooseVisual(dpy,DefaultScreen(dpy),(int[]){GLX_RGBA,None});
@@ -76,136 +74,134 @@ int main(int argc,char**argv){
 	pthread_attr_setguardsize(&pat,0);
 	pthread_attr_setinheritsched(&pat,PTHREAD_EXPLICIT_SCHED);
 	pthread_attr_setschedpolicy(&pat,SCHED_RR);
-    pthread_attr_setschedparam(&pat,(struct sched_param[]){{.sched_priority=sched_get_priority_max(SCHED_RR)}});
-    pthread_setschedparam(pthread_self(),SCHED_RR,(struct sched_param[]){{.sched_priority=sched_get_priority_min(SCHED_RR)}});
+	pthread_attr_setschedparam(&pat,(struct sched_param[]){{.sched_priority=sched_get_priority_max(SCHED_RR)}});
+	pthread_setschedparam(pthread_self(),SCHED_RR,(struct sched_param[]){{.sched_priority=sched_get_priority_min(SCHED_RR)}});
 	goto rend;
 	for(;;){
 		XEvent ev;
-		XNextEvent(dpy,&ev);
-		switch(ev.type){
-		case KeyPress:{
-			KeySym keysym;
-			if(XLookupString((XKeyEvent*)&ev,(char[]){0},1,&keysym,NULL)==1&&keysym==XK_Escape){
-				glXDestroyContext(dpy,glXGetCurrentContext());
-				return 0;
+		if(XPending(dpy)){
+			XNextEvent(dpy,&ev);
+			switch(ev.type){
+			case KeyPress:{
+				KeySym keysym;
+				if(XLookupString((XKeyEvent*)&ev,(char[]){0},1,&keysym,NULL)==1&&keysym==XK_Escape){
+					glXDestroyContext(dpy,glXGetCurrentContext());
+					return 0;
+				}
+			}
+			break;case Expose:
+				glBegin(GL_POINTS);
+				for(int i=0;i<512;i++)
+					for(int j=0;j<512;j++){
+						glColor3ub(manor[i][j]*manor[i][j]*manor[i][j]>>16,manor[i][j]*manor[i][j]>>8,manor[i][j]);
+						glVertex2i(i,j);
+					}
+				glEnd();
+				glFlush();
+			break;case ButtonPress:
+				switch(ev.xbutton.button){
+				default:goto rend;
+				case Button1:
+					if((unsigned)ev.xbutton.x>=512||(unsigned)ev.xbutton.y>=512)break;
+					nx=ev.xbutton.x;
+					ny=ev.xbutton.y;
+				break;case Button4:case Button5:
+					mxi+=ev.xbutton.button==Button4?25:mxi>25?-25:0;
+					printf("%d ",mxi);
+				}
+			break;case ButtonRelease:
+				if(ev.xbutton.button==Button1){
+					if((unsigned)ev.xbutton.x>=512||(unsigned)ev.xbutton.y>=512)break;
+					if(mans)
+						for(int i=0;i<THREADS;i++)pthread_join(a[i],0);
+					if(ev.xbutton.x==nx&&ev.xbutton.y==ny){
+						if(xs)mpn_addmul_1(xx,wh,pr,512-ev.xbutton.x);
+						else{
+							mp_limb_t t[pr*2];
+							mpn_mul_1(t,wh,pr,512-ev.xbutton.x);
+							if(mpn_cmp(xx,t,pr)<0){
+								xs=!xs;
+								mpn_sub_n(xx,t,xx,pr);
+							}else mpn_sub_n(xx,xx,t,pr);
+						}
+						if(ys)mpn_addmul_1(yy,wh,pr,512-ev.xbutton.y);
+						else{
+							mp_limb_t t[pr*2];
+							mpn_mul_1(t,wh,pr,512-ev.xbutton.y);
+							if(mpn_cmp(yy,t,pr)<0){
+								ys=!ys;
+								mpn_sub_n(yy,t,yy,pr);
+							}else mpn_sub_n(yy,yy,t,pr);
+						}
+						mpn_lshift(wh,wh,pr,1);
+					}else{
+						if(ev.xbutton.x<nx){
+							int t=ev.xbutton.x;
+							ev.xbutton.x=nx;
+							nx=t;
+						}
+						if(ev.xbutton.y<ny){
+							int t=ev.xbutton.y;
+							ev.xbutton.y=ny;
+							ny=t;
+						}
+						if(xs){
+							mp_limb_t t[pr*2];
+							mpn_mul_1(t,wh,pr,nx);
+							if(mpn_cmp(xx,t,pr)<0){
+								xs=!xs;
+								mpn_sub_n(xx,t,xx,pr);
+							}else mpn_sub_n(xx,xx,t,pr);
+						}else mpn_addmul_1(xx,wh,pr,nx);
+						if(ys){
+							mp_limb_t t[pr*2];
+							mpn_mul_1(t,wh,pr,ny);
+							if(mpn_cmp(yy,t,pr)<0){
+								ys=!ys;
+								mpn_sub_n(yy,t,yy,pr);
+							}else mpn_sub_n(yy,yy,t,pr);
+						}else mpn_addmul_1(yy,wh,pr,ny);
+						nx=ev.xbutton.x-nx;
+						ny=ev.xbutton.y-ny;
+						mpn_mul_1(wh,wh,pr,nx-(nx-ny&nx-ny>>sizeof(int)*8-1));
+						mpn_rshift(wh,wh,pr,9);
+						if(xx[0]||yy[0]||wh[0]){
+							pr++;
+							wh=(yy=(xx=realloc(xx,pr*3*sizeof(mp_limb_t)))+pr)+pr;
+							mpn_copyd(wh+1,wh-2,pr-1);
+							mpn_copyd(yy+1,yy-1,pr-1);
+							mpn_copyd(xx+1,xx,pr-1);
+							xx[yy[wh[0]=0]=0]=0;
+						}
+					}
+					rend:gmp_printf("%u\n%Nx\n%Nx\n%Nx\n",pr,xx,pr,yy,pr,wh,pr);
+					mans=THREADS-1;
+					for(int i=0;i<THREADS;i++){
+						n[i]=i;
+						pthread_create(a+i,&pat,drawman,manor+i);
+					}
+				}
 			}
 		}
-		break;case Expose:
-			glBegin(GL_POINTS);
-			for(int i=0;i<512;i++)
-				for(int j=0;j<512;j++){
-					glColor3f(manor[i][j]*manor[i][j]*manor[i][j]/16777216.,manor[i][j]*manor[i][j]/65536.,manor[i][j]/256.);
-					glVertex2i(i,j);
+		if(mans)
+			for(int i=0;i<THREADS;i++)
+				if(!pthread_tryjoin_np(a[i],0)){
+					int k=n[i];
+					if(++mans<512){
+						n[i]=mans;
+						pthread_create(a+i,&pat,drawman,manor+mans);
+					}else if(mans>=511+THREADS){
+						mans=0;
+						return 0;
+						break;
+					}
+					glBegin(GL_POINTS);
+					for(int j=0;j<512;j++){
+						glColor3ub(manor[k][j]*manor[k][j]*manor[k][j]>>16,manor[k][j]*manor[k][j]>>8,manor[k][j]);
+						glVertex2i(k,j);
+					}
+					glEnd();
+					glFlush();
 				}
-			glEnd();
-			glFlush();
-		break;case ButtonPress:
-			switch(ev.xbutton.button){
-			default:goto rend;
-			case Button1:
-				if((unsigned)ev.xbutton.x>512||(unsigned)ev.xbutton.y>512) break;
-				nx=ev.xbutton.x;
-				ny=ev.xbutton.y;
-			break;case Button4:case Button5:
-				mxi+=ev.xbutton.button==Button4?25:mxi>25?-25:0;
-				printf("%d ",mxi);
-			}
-		break;case ButtonRelease:
-			if(ev.xbutton.button==Button1){
-				if((unsigned)ev.xbutton.x>512||(unsigned)ev.xbutton.y>512) break;
-				if(ev.xbutton.x==nx&&ev.xbutton.y==ny){
-					if(xs){
-						mpn_addmul_1(xx,wh,pr,512-ev.xbutton.x);
-					}else{
-						mp_limb_t t[pr*2];
-						mpn_mul_1(t,wh,pr,512-ev.xbutton.x);
-						if(mpn_cmp(xx,t,pr)<0){
-							xs=!xs;
-							mpn_sub_n(xx,t,xx,pr);
-						}else mpn_sub_n(xx,xx,t,pr);
-					}
-					if(ys){
-						mpn_addmul_1(yy,wh,pr,512-ev.xbutton.y);
-					}else{
-						mp_limb_t t[pr*2];
-						mpn_mul_1(t,wh,pr,512-ev.xbutton.y);
-						if(mpn_cmp(yy,t,pr)<0){
-							ys=!ys;
-							mpn_sub_n(yy,t,yy,pr);
-						}else mpn_sub_n(yy,yy,t,pr);
-					}
-					mpn_lshift(wh,wh,pr,1);
-				}else{
-					if(ev.xbutton.x<nx){
-						int t=ev.xbutton.x;
-						ev.xbutton.x=nx;
-						nx=t;
-					}
-					if(ev.xbutton.y<ny){
-						int t=ev.xbutton.y;
-						ev.xbutton.y=ny;
-						ny=t;
-					}
-					if(!xs){
-						mpn_addmul_1(xx,wh,pr,nx);
-					}else{
-						mp_limb_t t[pr*2];
-						mpn_mul_1(t,wh,pr,nx);
-						if(mpn_cmp(xx,t,pr)<0){
-							xs=!xs;
-							mpn_sub_n(xx,t,xx,pr);
-						}else mpn_sub_n(xx,xx,t,pr);
-					}
-					if(!ys){
-						mpn_addmul_1(yy,wh,pr,ny);
-					}else{
-						mp_limb_t t[pr*2];
-						mpn_mul_1(t,wh,pr,ny);
-						if(mpn_cmp(yy,t,pr)<0){
-							ys=!ys;
-							mpn_sub_n(yy,t,yy,pr);
-						}else mpn_sub_n(yy,yy,t,pr);
-					}
-					nx=ev.xbutton.x-nx;
-					ny=ev.xbutton.y-ny;
-					mpn_mul_1(wh,wh,pr,nx-(nx-ny&nx-ny>>sizeof(int)*8-1));
-					mpn_rshift(wh,wh,pr,9);
-					if(xx[0]||yy[0]){
-						pr++;
-						xx=realloc(xx,pr*3*sizeof(mp_limb_t));
-						yy=xx+pr;
-						wh=yy+pr;
-						for(int i=pr-1;i;i--){
-							wh[i]=wh[i-3];
-							yy[i]=yy[i-2];
-							xx[i]=xx[i-1];
-						}
-						xx[0]=yy[0]=wh[0]=0;
-					}
-				}
-				rend:gmp_printf("%u\n%Nx\n%Nx\n%Nx\n",pr,xx,pr,yy,pr,wh,pr);
-				uint32_t n[THREADS],mans=THREADS-1;
-				for(int i=0;i<THREADS;i++){
-					n[i]=i;
-					pthread_create(a+i,&pat,drawman,manor+i);
-				}
-				while(mans<511+THREADS)
-					for(int i=0;i<THREADS;i++)
-						if(!pthread_tryjoin_np(a[i],0)){
-							int k=n[i];
-							if(++mans<512){
-								n[i]=mans;
-								pthread_create(a+i,&pat,drawman,manor+mans);
-							}
-							glBegin(GL_POINTS);
-							for(int j=0;j<512;j++){
-								glColor3ub(manor[k][j]*manor[k][j]*manor[k][j]>>16,manor[k][j]*manor[k][j]>>8,manor[k][j]);
-								glVertex2i(k,j);
-							}
-							glEnd();
-							glFlush();
-						}
-			}
-		}
 	}
 }
