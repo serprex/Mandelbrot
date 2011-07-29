c448201f02a2a2e890f68454e1fdc36265f9fe06
Demur Rumed <gunkmute@gmail.com>
2011-07-29 03:24:40 +0000

    Down with strings and sizeofs. Refactored estoupe for concurrent IO

--- estoupe.c
+++ estoupe.c
@@ -1,12 +1,6 @@
 #include <CL/cl.h>
 #include <stdio.h>
 #include <GL/glx.h>
-#include <string.h>
-float xx=-2,yy=-2,wh=1/128.;
-unsigned char manor[512][512];
-unsigned long long done[8];
-unsigned mxi=300;
-int col;
 #define CL_CHECK(_expr)do{cl_int _err=_expr;if(_err!=CL_SUCCESS)return printf("'%s' returned %d!\n",#_expr,_err);}while(0)
 #define CL_CHECK_ERR(_expr)({cl_int _err;typeof(_expr)_ret=_expr;if(_err!=CL_SUCCESS)return printf("'%s' returned %d!\n",#_expr,(int)_err);_ret;})
 int main(int argc,char**argv){
@@ -22,16 +16,20 @@ int main(int argc,char**argv){
 	cl_program pg=CL_CHECK_ERR(clCreateProgramWithSource(cx,1,&S,0,&_err));
 	if(clBuildProgram(pg,1,&dv,"",0,0)!=CL_SUCCESS){
 		char buffer[8192];
-		clGetProgramBuildInfo(pg,dv,CL_PROGRAM_BUILD_LOG,sizeof(buffer),buffer,0);
+		clGetProgramBuildInfo(pg,dv,CL_PROGRAM_BUILD_LOG,8192,buffer,0);
 		printf("%s",buffer);
 		return 1;
 	}
 	clUnloadCompiler();
 	cl_kernel k=CL_CHECK_ERR(clCreateKernel(pg,"m",&_err));
 	cl_command_queue q=CL_CHECK_ERR(clCreateCommandQueue(cx,dv,0,&_err));
-	cl_mem px=CL_CHECK_ERR(clCreateBuffer(cx,CL_MEM_WRITE_ONLY,sizeof(int)*128,0,&_err));
+	cl_mem px=CL_CHECK_ERR(clCreateBuffer(cx,CL_MEM_WRITE_ONLY,512,0,&_err));
 	clSetKernelArg(k,0,sizeof(cl_mem),&px);
-	int nx,ny;
+	const size_t gws=128;
+	float xx=-2,yy=-2,wh=1/128.;
+	unsigned char manor[512][512];
+	unsigned mxi=300;
+	int nx,ny,mans;
 	unsigned char C[256][3];
 	for(int i=0;i<256;i++){
 		C[i][0]=i*i*i>>16;
@@ -47,78 +45,83 @@ int main(int argc,char**argv){
 	goto rend;
 	for(;;){
 		XEvent ev;
-		xne:XNextEvent(dpy,&ev);
-		switch(ev.type){
-		case Expose:;
-			glBegin(GL_POINTS);
-			for(int i=ev.xexpose.x;i<=ev.xexpose.x+ev.xexpose.width;i++)
-				for(int j=ev.xexpose.y;j<=ev.xexpose.y+ev.xexpose.height;j++){
-					glColor3ubv(C[manor[i][j]]);
-					glVertex2i(i,j);
-				}
-			glEnd();
-			glFlush();
-		break;case ButtonPress:
-			switch(ev.xbutton.button){
-			default:goto rend;
-			case Button1:
-				if((unsigned)ev.xbutton.x>=512||(unsigned)ev.xbutton.y>=512)break;
-				case Button3:
-				nx=ev.xbutton.x;
-				ny=ev.xbutton.y;
-			break;case Button4:case Button5:mxi+=ev.xbutton.button==Button4?25:mxi>25?-25:0;
-			}
-		break;case ButtonRelease:
-			if(ev.xbutton.button==Button1&&(unsigned)ev.xbutton.x<512&&(unsigned)ev.xbutton.y<512){
-				if(ev.xbutton.x==nx&&ev.xbutton.y==ny){
-					xx+=(ev.xbutton.x-512)*wh;
-					yy+=(ev.xbutton.y-512)*wh;
-					wh*=2;
-				}else{
-					if(ev.xbutton.x<nx){
-						int t=ev.xbutton.x;
-						ev.xbutton.x=nx;
-						nx=t;
-					}
-					if(ev.xbutton.y<ny){
-						int t=ev.xbutton.y;
-						ev.xbutton.y=ny;
-						ny=t;
-					}
-					from3:
-					xx+=nx*wh;
-					yy+=ny*wh;
-					if(ev.xbutton.button==Button1){
-						printf("%d %d %d %d\n",ev.xbutton.x,nx,ev.xbutton.y,ny);
-						nx=ev.xbutton.x-nx;
-						ny=ev.xbutton.y-ny;
-						wh*=(nx-(nx-ny&nx-ny>>sizeof(int)*8-1))/512.;
-					}
-				}
-				rend:printf("%u\n%f\n%f\n%f\n\n",mxi,xx,yy,wh);
-				clSetKernelArg(k,2,sizeof(float),&yy);
-				clSetKernelArg(k,3,sizeof(float),&wh);
-				clSetKernelArg(k,4,sizeof(int),&mxi);
-				float x=xx;
-				for(int i=0;i<512;i++){
-					x+=wh;
-					clSetKernelArg(k,1,sizeof(float),&x);
-					size_t gws=128;
-					CL_CHECK(clEnqueueNDRangeKernel(q,k,1,0,&gws,0,0,0,0));
-					clFinish(q);
-					CL_CHECK(clEnqueueReadBuffer(q,px,CL_TRUE,0,128*sizeof(int),manor[i],0,0,0));
-					glBegin(GL_POINTS);
-					for(int j=0;j<512;j++){
+		if(XPending(dpy)||mans==512){
+			xne:XNextEvent(dpy,&ev);
+			switch(ev.type){
+			case Expose:;
+				glBegin(GL_POINTS);
+				for(int i=ev.xexpose.x;i<=ev.xexpose.x+ev.xexpose.width;i++)
+					for(int j=ev.xexpose.y;j<=ev.xexpose.y+ev.xexpose.height;j++){
 						glColor3ubv(C[manor[i][j]]);
 						glVertex2i(i,j);
 					}
-					glEnd();
-				}
+				glEnd();
 				glFlush();
-			}else if(ev.xbutton.button==Button3){
-				nx-=ev.xbutton.x;
-				ny-=ev.xbutton.y;
-				goto from3;
+			break;case ButtonPress:
+				switch(ev.xbutton.button){
+				default:goto rend;
+				case Button1:
+					if((unsigned)ev.xbutton.x>=512||(unsigned)ev.xbutton.y>=512)break;
+					case Button3:
+					nx=ev.xbutton.x;
+					ny=ev.xbutton.y;
+				break;case Button4:case Button5:mxi+=ev.xbutton.button==Button4?25:mxi>25?-25:0;
+				}
+			break;case ButtonRelease:
+				if(ev.xbutton.button==Button1&&(unsigned)ev.xbutton.x<512&&(unsigned)ev.xbutton.y<512){
+					if(ev.xbutton.x==nx&&ev.xbutton.y==ny){
+						xx+=(ev.xbutton.x-512)*wh;
+						yy+=(ev.xbutton.y-512)*wh;
+						wh*=2;
+					}else{
+						if(ev.xbutton.x<nx){
+							int t=ev.xbutton.x;
+							ev.xbutton.x=nx;
+							nx=t;
+						}
+						if(ev.xbutton.y<ny){
+							int t=ev.xbutton.y;
+							ev.xbutton.y=ny;
+							ny=t;
+						}
+						from3:
+						xx+=nx*wh;
+						yy+=ny*wh;
+						if(ev.xbutton.button==Button1){
+							printf("%d %d %d %d\n",ev.xbutton.x,nx,ev.xbutton.y,ny);
+							nx=ev.xbutton.x-nx;
+							ny=ev.xbutton.y-ny;
+							wh*=(nx-(nx-ny&nx-ny>>31))/512.;
+						}
+					}
+					rend:printf("%u\n%f\n%f\n%f\n\n",mxi,xx,yy,wh);
+					mans=0;
+					clSetKernelArg(k,1,4,&xx);
+					clSetKernelArg(k,2,4,&yy);
+					clSetKernelArg(k,3,4,&wh);
+					clSetKernelArg(k,4,4,&mxi);
+					CL_CHECK(clEnqueueNDRangeKernel(q,k,1,0,&gws,0,0,0,0));
+				}else if(ev.xbutton.button==Button3){
+					nx-=ev.xbutton.x;
+					ny-=ev.xbutton.y;
+					goto from3;
+				}
+			}
+		}
+		if(mans!=512){
+			clFinish(q);
+			CL_CHECK(clEnqueueReadBuffer(q,px,CL_TRUE,0,512,manor[mans],0,0,0));
+			glBegin(GL_POINTS);
+			for(int j=0;j<512;j++){
+				glColor3ubv(C[manor[mans][j]]);
+				glVertex2i(mans,j);
+			}
+			glEnd();
+			if(++mans==512)glFlush();
+			else{
+				float x=xx+mans*wh;
+				clSetKernelArg(k,1,4,&x);
+				CL_CHECK(clEnqueueNDRangeKernel(q,k,1,0,&gws,0,0,0,0));
 			}
 		}
 	}
